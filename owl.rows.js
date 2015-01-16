9a149a0dd9ed6d99bff4a774c378864bd89775fc
BBoyJuss <yunus.gaziev@gmail.com>
2015-01-17 01:47:12 +0500

    Added Initialize handler

--- owl.rows.js
+++ owl.rows.js
@@ -6,14 +6,25 @@
     Rows = function(scope){
         this.owl = scope;
         this.owl._options = $.extend(Rows.Defaults, this.owl.options);
-        //link callback events with owl carousel here
+
+        this.handlers = {
+            'initialize.owl.carousel' : $.proxy(function (e) {
+                if (this.owl._options.rows) {
+                    this.startup();
+                }
+            }, this)
+        };
+
+        this.owl.dom.$el.on(this.handlers);
     }
+
     Rows.Defaults = {
         rows: true
     }
 
     //mehtods:
-    Rows.prototype.method = function(){
+    Rows.prototype.startup = function(){
+        console.info(this);
     }
 
     //destroy:
