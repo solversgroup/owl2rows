2aa7a17df8a368d3c105d9ace167d368a85e96c9
Yunus <yunusga@users.noreply.github.com>
2016-03-14 16:08:18 +0500

    Update owl.rows.js

--- owl.rows.js
+++ owl.rows.js
@@ -1,63 +1,139 @@
 /**
- * Owl Rows
- * @since 2.0.0
+ * Owl2row
+ * @since 2.0.1
  */
-;(function ( $, window, document, undefined ) {
-    Rows = function(scope){
+;(function ($, window, document, undefined) {
+    Owl2row = function (scope) {
         this.owl = scope;
-        this.owl.options = $.extend(Rows.Defaults, this.owl.options);
-
-        this.carousel = {};
-        this.sourceItems = {};
+        this.owl.options = $.extend(Owl2row.Defaults, this.owl.options);
+        //link callback events with owl carousel here
 
         this.handlers = {
-            'initialize.owl.carousel' : $.proxy(function (e) {
-                if (this.owl.options.rows) {
-                    this.startup();
+            'initialize.owl.carousel': $.proxy(function (e) {
+                if (this.owl.settings.owl2row) {
+                    this.build2row(this);
                 }
             }, this)
         };
 
-        this.owl.dom.$el.on(this.handlers);
-    }
+        this.owl.$element.on(this.handlers);
+    };
 
-    Rows.Defaults = {
-        rows: true,
-        rowsCount: 2,
-        rowsTargetClass: 'item'
-    }
+    Owl2row.Defaults = {
+        owl2row: 'true',
+        owl2rowTarget: 'item',
+        owl2rowContainer: 'owl2row-item',
+        owl2rowDirection: 'utd' // ltr
+    };
 
     //mehtods:
-    Rows.prototype.startup = function(){
+    Owl2row.prototype.build2row = function(thisScope){
+    
+        var carousel = $(thisScope.owl.$element);
+        var carouselItems = carousel.find('.' + thisScope.owl.options.owl2rowTarget);
 
-        var carousel = $('.' + this.owl.options.baseClass),
-            column = $('<div/>'),
-            rowsCount = this.owl.options.rowsCount,
-            counter = 0;
+        var aEvenElements = [];
+        var aOddElements = [];
 
-        this.sourceItems = carousel.find('.' + this.owl.options.rowsTargetClass);
+        $.each(carouselItems, function (index, item) {
+            if ( index % 2 === 0 ) {
+                aEvenElements.push(item);
+            } else {
+                aOddElements.push(item);
+            }
+        });
 
         carousel.empty();
 
-        $.each(this.sourceItems, function(i, el) {
-            counter++;
+        switch (thisScope.owl.options.owl2rowDirection) {
+            case 'ltr':
+                thisScope.leftToright(thisScope, carousel, carouselItems);
+                break;
 
-            if (counter < rowsCount) {
-                column.append(el);
-            } else if (counter = rowsCount) {
-                column.append(el);
-                carousel.append(column);
-                column = $('<div/>');
-                counter = 0;
-            }
+            default :
+                thisScope.upTodown(thisScope, aEvenElements, aOddElements, carousel);
+        }
 
-
-        });
-    }
-
-    //destroy:
-    Rows.prototype.destroy = function(){
-        //events here
     };
-    $.fn.owlCarousel.Constructor.Plugins['rows'] = Rows;
+
+    Owl2row.prototype.leftToright = function(thisScope, carousel, carouselItems){
+
+        var o2wContainerClass = thisScope.owl.options.owl2rowContainer;
+        var owlMargin = thisScope.owl.options.margin;
+
+        var carouselItemsLength = carouselItems.length;
+
+        var firsArr = [];
+        var secondArr = [];
+
+        //console.log(carouselItemsLength);
+
+        if (carouselItemsLength %2 === 1) {
+            carouselItemsLength = ((carouselItemsLength - 1)/2) + 1;
+        } else {
+            carouselItemsLength = carouselItemsLength/2;
+        }
+
+        //console.log(carouselItemsLength);
+
+        $.each(carouselItems, function (index, item) {
+
+
+            if (index < carouselItemsLength) {
+                firsArr.push(item);
+            } else {
+                secondArr.push(item);
+            }
+        });
+
+        $.each(firsArr, function (index, item) {
+            var rowContainer = $('<div class="' + o2wContainerClass + '"/>');
+
+            var firstRowElement = firsArr[index];
+                firstRowElement.style.marginBottom = owlMargin + 'px';
+
+            rowContainer
+                .append(firstRowElement)
+                .append(secondArr[index]);
+
+            carousel.append(rowContainer);
+        });
+
+    };
+
+    Owl2row.prototype.upTodown = function(thisScope, aEvenElements, aOddElements, carousel){
+
+        var o2wContainerClass = thisScope.owl.options.owl2rowContainer;
+        var owlMargin = thisScope.owl.options.margin;
+
+        $.each(aEvenElements, function (index, item) {
+
+            var rowContainer = $('<div class="' + o2wContainerClass + '"/>');
+            var evenElement = aEvenElements[index];
+
+            evenElement.style.marginBottom = owlMargin + 'px';
+
+            rowContainer
+                .append(evenElement)
+                .append(aOddElements[index]);
+
+            carousel.append(rowContainer);
+        });
+    };
+
+    /**
+     * Destroys the plugin.
+     */
+    Owl2row.prototype.destroy = function() {
+        var handler, property;
+
+        for (handler in this.handlers) {
+            this.owl.dom.$el.off(handler, this.handlers[handler]);
+        }
+        for (property in Object.getOwnPropertyNames(this)) {
+            typeof this[property] !== 'function' && (this[property] = null);
+        }
+    };
+
+    $.fn.owlCarousel.Constructor.Plugins['owl2row'] = Owl2row;
 })( window.Zepto || window.jQuery, window,  document );
